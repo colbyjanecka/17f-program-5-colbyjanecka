a702af82c5eafee17586a86dd0ba45ada95d50c6
Colby Janecka <colbyjanecka@aol.com>
2017-12-10 20:38:08 -0600

    beautified code

--- Main.asm
+++ Main.asm
@@ -15,104 +15,104 @@
 
 				LD 			R1, IVT_ADDRESS		; Load address of keyboard interrupt vector location
 				LD			R2, ISR_ADDRESS		; 		..
-				STR 		R2, R1, #0			; and store the ISR origin address at the IVT address.
+				STR 			R2, R1, #0		; and store the ISR origin address at the IVT address.
 
 ; enable keyboard interrupts
 
 				LD			R1, KBSR_ADDRESS	; Load address of the KBSR register
-				LDR 		R2, R1, #2			; Load contents of the KBDR register (at xFE02, in order to clear input data)
+				LDR 			R2, R1, #2		; Load contents of the KBDR register (at xFE02, in order to clear input data)
 				LD			R3, IntEnable		; Load the value x4000, which
 				STR			R3, R1, #0			;								sets the interrupt enable bit at KBSR[14] to 1.
 
 ; start of actual program
 
-START 			JSR			GET_LETTER
+START 				JSR			GET_LETTER
 				JSR			PRINT_LETTER
 				LD			R3, A				; If input is A, continue to START2
-				ADD 		R3, R3, R0			;		..
+				ADD 			R3, R3, R0			;		..
 				BRz			START2				;		..
 				LD			R0, LETTER			; Verify no changes to input letter stored in R0
-				BRnp		START				; If input is not A, return back to START
+				BRnp			START				; If input is not A, return back to START
 				BR			START				; Else, return to START
 
-START2			JSR			GET_LETTER
+START2				JSR			GET_LETTER
 				JSR			PRINT_LETTER
 				LD			R3, U				; If input is U, continue to START3
-				ADD 		R3, R3, R0			;		..
+				ADD 			R3, R3, R0			;		..
 				BRz			START3				;		..
 				LD			R3, A				; If input is A, keep pollng for U
-				ADD 		R3, R3, R0			;		..
-				BRz 		START2				;		..
+				ADD 			R3, R3, R0			;		..
+				BRz 			START2				;		..
 				LD			R0, LETTER			; Verify no changes to input letter stored in R0
-				BRnp		START				; If input is not A/U, return back to START
+				BRnp			START				; If input is not A/U, return back to START
 				BR			START2				; Else, return to START2
 
-START3			JSR			GET_LETTER
+START3				JSR			GET_LETTER
 				JSR			PRINT_LETTER
 				LD			R3, G				; If input is G, continue to SEQUENCE
-				ADD 		R3, R3, R0			;		..
+				ADD 			R3, R3, R0			;		..
 				BRz			SEQUENCE			;		..
 				LD			R3, A				; If input is A, continue to START2
-				ADD 		R3, R3, R0			;		..
+				ADD 			R3, R3, R0			;		..
 				BRz			START2				;		..
 				LD			R0, LETTER			; Verify no changes to input letter stored in R0
-				BRnp		START				; If input is not G, return back to START
+				BRnp			START				; If input is not G, return back to START
 				BR			START3				; Else, return to START3
 
-SEQUENCE		LD			R0, BAR
+SEQUENCE			LD			R0, BAR
 				OUT
 
-SEQLOOP			JSR			GET_LETTER
+SEQLOOP				JSR			GET_LETTER
 				JSR			PRINT_LETTER
 				LD			R3, U				; If input is U, continue to END1
-				ADD 		R3, R3, R0			;		..
+				ADD 			R3, R3, R0			;		..
 				BRz			END1				;		..
 				LD			R0, LETTER			; Verify no changes to input letter stored in R0
 				BR			SEQLOOP				; Else, return to SEQLOOP
 
-END1			JSR			GET_LETTER
+END1				JSR			GET_LETTER
 				JSR			PRINT_LETTER
 				LD			R3, A				; If input is A, go to END2
-				ADD 		R3, R3, R0			;		..
+				ADD 			R3, R3, R0			;		..
 				BRz			END2				;		..
 				LD			R3, G				; If input is G, go to END3
-				ADD 		R3, R3, R0			;		..
-				BRz 		END3				;		..
+				ADD 			R3, R3, R0			;		..
+				BRz 			END3				;		..
 				LD			R3, U				; If input is U, keep polling for A/G
 				ADD			R3, R3, R0			;		..
-				BRz 		END1				;		..
+				BRz 			END1				;		..
 				LD			R0, LETTER			; Verify no changes to input letter stored in R0
-				BRnp		SEQLOOP				; If input is not G/A/U, return back to SEQLOOP
+				BRnp			SEQLOOP				; If input is not G/A/U, return back to SEQLOOP
 				BR			END1				; Else, return to END1
 
-END2			JSR			GET_LETTER
+END2				JSR			GET_LETTER
 				JSR			PRINT_LETTER
 				LD			R3, G				; If input is G, continue to END
-				ADD 		R3, R3, R0			;		..
+				ADD 			R3, R3, R0			;		..
 				BRz			END					;		..
 				LD			R3, A				; If input is A, continue to END
-				ADD 		R3, R3, R0			;		..
-				BRz 		END					;		..
+				ADD 			R3, R3, R0			;		..
+				BRz 			END				;		..
 				LD			R3, U				; If input is U, continue to END1
-				ADD 		R3, R3, R0			;		..
-				BRz 		END1				;		..
+				ADD 			R3, R3, R0			;		..
+				BRz 			END1				;		..
 				LD			R0, LETTER			; Verify no changes to input letter stored in R0
-				BRnp		SEQLOOP				; If input is not G/A, return back to SEQLOOP
+				BRnp			SEQLOOP				; If input is not G/A, return back to SEQLOOP
 				BR			END2				; Else, return to END2
 
-END3			JSR			GET_LETTER
+END3				JSR			GET_LETTER
 				JSR			PRINT_LETTER
 				LD			R3, A				; If input is A, continue to END
-				ADD 		R3, R3, R0			;		..
-				BRz 		END					;		..
+				ADD 			R3, R3, R0			;		..
+				BRz 			END				;		..
 				LD			R3, U				; If input is U, continue to END1
-				ADD 		R3, R3, R0			;		..
-				BRz 		END1				;		..
+				ADD 			R3, R3, R0			;		..
+				BRz 			END1				;		..
 				LD			R0, LETTER			; Verify no changes to input letter stored in R0
-				BRnp		SEQLOOP				; If input is not A, return back to SEQLOOP
+				BRnp			SEQLOOP				; If input is not A, return back to SEQLOOP
 				BR			END3				; Else, return to END3
 
-END 			HALT							; If END codon is recieved, then halt the program.
+END 				HALT							; If END codon is recieved, then halt the program.
 
 
 ; ********************
@@ -121,8 +121,8 @@ END 			HALT							; If END codon is recieved, then halt the program.
 ; Gets the current value stored in x2600 and loads it into R0
 ; then saves it for later access at the label LETTER.
 ;*********************
-GET_LETTER		ST			R7, SAVE70			; Save current RET value
-				LD			R1, Char_Address	; Load x2600 into R1
+GET_LETTER			ST			R7, SAVE70			; Save current RET value
+				LD			R1, Char_Address		; Load x2600 into R1
 				LDR			R0, R1, #0			; Load value stored at this memory location
 				ST			R0, LETTER			; And save it in the LETTER label
 				LD			R7, SAVE70			; Load current RET value
@@ -136,12 +136,12 @@ GET_LETTER		ST			R7, SAVE70			; Save current RET value
 ; Prints the current letter that is stored at the LETTER label,
 ; and sets the value in M[x2600] to #0 (x0000)
 ;*********************
-PRINT_LETTER	ST			R7, SAVE71			; Save current RET value
+PRINT_LETTER			ST			R7, SAVE71			; Save current RET value
 				LD			R0, LETTER			; Load current input letter
-				BRz 		#5					; If none, skip printing
+				BRz 			#5				; If none, skip printing
 
 				ST			R1, SAVE1			; Save R1, which is used by OUT trap routine
-				OUT								; Perform printing of letter
+				OUT							; Perform printing of letter
 				LD			R1, SAVE1			; Load previous value for R1
 
 				AND			R5, R5, #0			; Set 0 -> M[x2600]
@@ -150,21 +150,21 @@ PRINT_LETTER	ST			R7, SAVE71			; Save current RET value
 
 				RET
 
-BAR 			.FILL		x007C
-Char_Address	.FILL		x2600
-IVT_ADDRESS		.FILL		x0180
-ISR_ADDRESS		.FILL		x2500
-KBSR_ADDRESS	.FILL		xFE00
-KBDR 			.FILL		xFE02
-IntEnable		.FILL		x4000
-SAVE1			.FILL		x0000
-SAVE70			.FILL		x0000
-SAVE71			.FILL		x0000
+BAR 				.FILL		x007C
+Char_Address			.FILL		x2600
+IVT_ADDRESS			.FILL		x0180
+ISR_ADDRESS			.FILL		x2500
+KBSR_ADDRESS			.FILL		xFE00
+KBDR 				.FILL		xFE02
+IntEnable			.FILL		x4000
+SAVE1				.FILL		x0000
+SAVE70				.FILL		x0000
+SAVE71				.FILL		x0000
 A				.FILL		xFFBF
 C				.FILL		xFFBD
 G				.FILL		xFFB9
 U				.FILL		xFFAB
-LETTER 			.FILL		x0000
+LETTER 				.FILL		x0000
 
 
 				.END
